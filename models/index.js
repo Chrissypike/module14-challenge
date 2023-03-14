8e2758ae1b87f12b533d4bbaafa739bd2b228a04
Chrissypike <you@example.com>
2023-03-14 11:38:53 -0500

    importing module files through index.js

---
+++ models/index.js
@@ -0,0 +1,32 @@
+//importing all the model files
+const Comments = require('./comments');
+const Posts = require('./posts');
+const User = require('./user');
+
+User.hasMany(Posts, {
+    foreignKey: 'user_id',
+});
+
+Posts.belongs(User, {
+    foreignKey: 'user_id',
+});
+
+Comments.BelongsToMany(User, Posts, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+    hooks: true,
+});
+
+User.hasMany(Comments, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+    hooks: true,
+});
+
+Posts.hasMany(Comments, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
+    hooks: true,
+})
+
+module.exports = { Comments, Posts, User };
