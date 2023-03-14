f8084ec0210c05cff3036459c47dddaaf10bc8d5
Chrissypike <you@example.com>
2023-03-14 11:31:51 -0500

    routes.js files added

---
+++ controllers/home-routes.js
@@ -0,0 +1,51 @@
+const router = require('express').Router();
+const sequalize = require('../config/connection');
+const { Posts, User, Comments } = require('../models');
+const posts = require('../models/posts');
+
+router.get('/', (req, res) => {
+    Posts.findAll({
+        attributes: [
+            'id',
+            'caption',
+            'text',
+            'created_at',
+        ],
+        include: [{
+            model: Comments,
+            attributes: ['id', 'comments_content', 'posts_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['user_name']
+            }
+        },
+        {
+            model: User,
+            attributes: ['user_name']
+        }
+    ]
+    })
+    .then(dbPostsData => {
+        const post = dbPostsData.map(posts => posts.get({ plain: true }));
+
+        res.render('homepage', { post, loggedIn: req.session.loggedIn});
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/login', (req, res) => {
+    Posts.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'caption',
+            'text',
+            'created_at',
+        ]
+    })
+})  
